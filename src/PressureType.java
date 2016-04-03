eadbbb2bd2ac693804149a9508a992291c219354
empoluboyarov <poluboyarovem@gmail.com>
2016-04-03 22:33:44 +0300

    Day 2

---
+++ src/PressureType.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Evgeniy on 03.04.2016.
+ */
+public enum PressureType {
+
+    ATM,
+    MPA,
+    MMPT,
+    BAR,
+    KGF
+}
--- src/Test.java
+++ src/Test.java
@@ -3,16 +3,6 @@
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Относительная плотность при 0.685 = "+ BaseCounts.relativDensity(0.685));
-
-        System.out.println("Коэффициент сжимаемости при "+ BaseCounts.compressibilityFactor(21,65,0.685));
-
-        System.out.println("Геометрический объем  "+ BaseCounts.supplyOfGas(21.2,56,0.686,10000,1420));
-
-        System.out.println();
-
-        System.out.println();
-
 
     }
 }

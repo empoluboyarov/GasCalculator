2afaadb19476c805b948050aa723914f4e365866
empoluboyarov <poluboyarovem@gmail.com>
2016-04-05 13:37:24 +0300

    Finished DewPoint, PressureConverter and 50% Base Counts

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,14 @@
+import java.util.List;
+
 /**
  * Created by Evgeniy on 31.03.2016.
  */
 public class Test {
     public static void main(String[] args) {
+
+       System.out.println(DewPoint.calcDP(14.3,51.6));
+
+        System.out.println(PressureConverter.convertPressure(51.2,PressureType.MPA,PressureType.KGF));
 
     }
 }
